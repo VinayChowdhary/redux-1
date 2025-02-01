cf2557d1feafc13db1661ec1a4ab31c39faa8942
VinayChowdhary <vinayk.polina@gmail.com>
2025-02-01 22:33:04 +0530

    redux update 2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return (
     <>
-      <h1>This is Heading</h1>
+      <h1>Redux Example </h1>
+      <p>Open Console or use Redux Dev Tools to see the change in state.</p>
       
     </>
   );
