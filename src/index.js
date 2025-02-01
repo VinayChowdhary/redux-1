5a51c9eb9f43825d8d5ef5233a619b7eb1742278
VinayChowdhary <vinayk.polina@gmail.com>
2025-02-01 22:29:38 +0530

    store update 1

--- src/index.js
+++ src/index.js
@@ -3,6 +3,32 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {legacy_createStore as  createStore} from "redux";
+import {composeWithDevTools} from "redux-devtools-extension";
+
+//initial state
+const defaultState = 0;
+
+//reducer
+const balReducer = (state=defaultState,action)=>{
+  console.log("reducer",action);
+  switch(action.type){
+    case 'DEPOSIT' : return state+100;
+    case 'WITHDRAW' : return state-100;
+    default : return state;
+  }
+}
+
+//creating store
+var store = createStore(balReducer,composeWithDevTools());
+
+console.log(store.getState());
+store.dispatch({type:'DEPOSIT'});
+console.log(store.getState());
+store.dispatch({type:'DEPOSIT'});
+console.log(store.getState());
+store.dispatch({type:'WITHDRAW'});
+console.log(store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
